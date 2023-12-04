bf870750f9a4f63472df0ed7427da19cde9d58de
josepbf <72404735+josepbf@users.noreply.github.com>
2023-12-04 16:15:36 +0100

    another bug

--- EL_dataset.py
+++ EL_dataset.py
@@ -178,17 +178,17 @@ class PVDefectsDS(torch.utils.data.Dataset):
           # Train
           for i in range(len(split_train_val_test_csv)):
             if split_train_val_test_csv.at[i,'train_val_test_split'] == 0:
-              self.split_imgs_name.append(split_train_val_test_csv.at[i,name_image])
+              self.split_imgs_name.append(split_train_val_test_csv.at[i,"name_image"])
         elif train_val_test == 1:
           # Validation
           for i in range(len(self.split_train_val_test_csv)):
             if split_train_val_test_csv.at[i,'train_val_test_split'] == 1:
-              self.split_imgs_name.append(split_train_val_test_csv.at[i,name_image])
+              self.split_imgs_name.append(split_train_val_test_csv.at[i,"name_image"])
         elif train_val_test == 2:
           # Test
           for i in range(len(self.split_train_val_test_csv)):
             if split_train_val_test_csv.at[i,'train_val_test_split'] == 2:
-              self.split_imgs_name.append(split_train_val_test_csv.at[i,name_image])
+              self.split_imgs_name.append(split_train_val_test_csv.at[i,"name_image"])
 
     def __getitem__(self, idx):
         # define paths
