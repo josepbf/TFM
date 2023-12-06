fcb6817ed70b7cd659e0bfb4f4381788c736baff
josepbf <72404735+josepbf@users.noreply.github.com>
2023-12-06 17:41:43 +0100

    self added to functions

--- EL_metrics.py
+++ EL_metrics.py
@@ -18,15 +18,15 @@ class Writer:
       wandb.init(project=name_experiment, config = config)
 
 
-   def store_metric(metric_name, scalar, step):
+   def store_metric(self, metric_name, scalar, step):
       wandb.log({metric_name: scalar}, step=step)
    
-   def get_train_or_val():
+   def get_train_or_val(self):
       return self.train_or_val
 
-   def store_matrix(matrix_name, matrix, step):
+   def store_matrix(self, matrix_name, matrix, step):
       wandb.log({matrix_name: matrix.numpy()}, step=step)
 
-   def close_writer():
+   def close_writer(self):
       wandb.finish()
       
