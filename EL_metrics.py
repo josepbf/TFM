14dab7277ce74abdfe124d6f12ad51375d53e7f4
josepbf <72404735+josepbf@users.noreply.github.com>
2023-12-06 11:32:54 +0100

    wandb init good

--- EL_metrics.py
+++ EL_metrics.py
@@ -4,22 +4,18 @@ import torch
 import numpy
 
 class Writer:
-   def __init__(self, name_experiment, train_or_val):
+   def __init__(self, name_experiment, train_or_val, config):
       self.name_experiment = name_experiment
       self.train_or_val = train_or_val # 0 for train, 1 for val
       if len(self.name_experiment) == 0:
-         self.name_experiment = 'defaultname'
+         self.name_experiment = 'finding_bugs'
 
       if self.train_or_val == 0:
          folder_name = 'training'
       else:
          folder_name = 'validation'
-      
-      now = datetime.now()
-      dt_string = now.strftime("%d_%m_%Y_%H_%M_%S")
 
-      run_name = f'{name_experiment}_{dt_string}_{folder_name}'
-      wandb.init(name=run_name, project=name_experiment)
+      wandb.init(project=name_experiment, config = config)
 
 
    def store_metric(metric_name, scalar, step):
