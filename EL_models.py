80fc382570cd39cf7c1892b9ee65e9f8e3f94850
josepbf <72404735+josepbf@users.noreply.github.com>
2023-12-22 01:47:06 +0100

    label smoothing scalable

--- EL_models.py
+++ EL_models.py
@@ -3,6 +3,7 @@ import torchvision
 import torch
 from datetime import datetime
 import torch.nn.functional as F
+from EL_train_utils import fastrcnn_loss_custom
 
 class Model:
     def __init__(self, 
@@ -27,6 +28,9 @@ class Model:
                 num_classes=num_classes, 
                 pretrained_backbone=pretrained_backbone, 
                 trainable_backbone_layers=trainable_backbone_layers) # Valid values are between 0 and 5
+
+            if label_smoothing > 0:
+                self.model.roi_heads.fastrcnn_loss = fastrcnn_loss_custom
 
         elif self.model_name == 'FasterRCNN_ResNet-50-FPN_v2':    
             # Constructs an improved Faster R-CNN model with a ResNet-50-FPN 
@@ -84,46 +88,6 @@ class Model:
         now = datetime.now()
         dt_string = now.strftime("%d_%m_%Y_%H_%M_%S")
         name_to_save = str("./" + self.model_name + "_epoch_" + str(epoch) + dt_string + ".pth")
-        torch.save(net, name_to_save)
-
-    def add_label_smoothing(self):
-        self.model.roi_heads.fastrcnn_loss = fastrcnn_loss_custom
-
-    def fastrcnn_loss_custom(class_logits, box_regression, labels, regression_targets):
-        # type: (Tensor, Tensor, List[Tensor], List[Tensor]) -> Tuple[Tensor, Tensor]
-        """
-        Computes the loss for Faster R-CNN.
-
-        Args:
-            class_logits (Tensor)
-            box_regression (Tensor)
-            labels (list[BoxList])
-            regression_targets (Tensor)
-
-        Returns:
-            classification_loss (Tensor)
-            box_loss (Tensor)
-        """
-
-        labels = torch.cat(labels, dim=0)
-        regression_targets = torch.cat(regression_targets, dim=0)
-
-        classification_loss = F.cross_entropy(class_logits, labels, self.label_smoothing)
-
-        # get indices that correspond to the regression targets for
-        # the corresponding ground truth labels, to be used with
-        # advanced indexing
-        sampled_pos_inds_subset = torch.where(labels > 0)[0]
-        labels_pos = labels[sampled_pos_inds_subset]
-        N, num_classes = class_logits.shape
-        box_regression = box_regression.reshape(N, box_regression.size(-1) // 4, 4)
-
-        box_loss = F.smooth_l1_loss(
-            box_regression[sampled_pos_inds_subset, labels_pos],
-            regression_targets[sampled_pos_inds_subset],
-            beta=1 / 9,
-            reduction="sum",
-        )
-        box_loss = box_loss / labels.numel()
+        torch.save(net, name_to_save)        
 
         return classification_loss, box_loss
