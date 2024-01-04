d09678ce2e5cf6b4bab4729b1362cb7cdedea7d9
josepbf <72404735+josepbf@users.noreply.github.com>
2024-01-04 10:36:39 +0100

    memory update and new max experiment bc last experiment crasher bc we surpass max storage

--- jobscript.sh
+++ jobscript.sh
@@ -11,7 +11,7 @@
 ### -- set walltime limit: hh:mm --  maximum 24 hours for GPU-queues right now
 #BSUB -W 24:00
 # request 5GB of system-memory
-#BSUB -R "rusage[mem=5GB]"
+#BSUB -R "rusage[mem=10GB]"
 ### -- Specify the output and error file. %J is the job-id --
 ### -- -o and -e mean append, -oo and -eo mean overwrite --
 #BSUB -o gpu_%J.out
@@ -28,6 +28,6 @@ nvidia-smi
 
 source ../myVirtEnv/bin/activate
 
-/appl/python/3.11.4/bin/python3.11 EL_train.py --save_model_and_optim_epochs 5 --run_id 0 --continue_trainings 0 --epoch_continue_from 0 --num_epochs 100 --data_imbalance_handler 1 --evaluate_every_epochs 5 --experiment_name test_max_num_epochs_24h_0
+/appl/python/3.11.4/bin/python3.11 EL_train.py --save_model_and_optim_epochs 5 --run_id 0 --continue_trainings 0 --epoch_continue_from 0 --num_epochs 100 --data_imbalance_handler 1 --evaluate_every_epochs 5 --experiment_name test_max_num_epochs_24h_1
 
 deactivate
